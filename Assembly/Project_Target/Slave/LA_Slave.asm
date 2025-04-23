3b36f1e567966103b8487f6ba5838054e96963d6
andrew1593571 <andrewkeller2002@gmail.com>
2025-04-23 09:26:48 -0600

    Update comments and file headers

--- Assembly/Project_Target/Slave/LA_Slave.asm
+++ Assembly/Project_Target/Slave/LA_Slave.asm
@@ -1,21 +1,16 @@
 ;************************************************************************************
 ;										    *
 ;   Filename:	    LA_Slave.asm						    *
-;   Date:	    November 5, 2024						    *
-;   File Version:   1								    *
+;   Date:	    April 23, 2025						    *
+;   File Version:   2								    *
 ;   Author:	    Alex Wheelock and Andrew Keller				    *
 ;   Company:	    Idaho State University					    *
-;   Description:    Assembly file for Laser Shooting Game gun. A gun that can fire  *
-;		    a laser at two frequencies (38kHz & 56kHz), with firing mode    *
-;		    select (single-shot, three round burst, continuous). Different  *
-;		    frequencies will be used to determine between player 1 (38kHz)  *
-;		    and 2 (56kHz) for scoring. Gun will use 12V (with solenoid), or *
-;		    5V (without solenoid), can also use 3.3V if using the LF PIC,   *
-;		    with the adjustment of some resistors for the solenoid current. *
-;										    *
-;		    Uses CCP1 (RC2) for laser PWM, RC3 for solenoid control. RB0 for*
-;		    trigger, RB1 for frequency select and RB3:2 used to select from *
-;		    the firing modes.						    *
+;   Description:    Assembly file for the target slave for a laser shooting arcade. *
+;		    Communicates with the Master via I2C. May be enabled or	    *
+;		    disabled using I2C commands. When an I2C read is executed, the  *
+;		    slave responds with the current status of the target. The I2C   *
+;		    address is set during PIC setup utilizing switches connected    *
+;		    to PORTA.							    *
 ;										    *
 ;************************************************************************************
 
@@ -24,13 +19,16 @@
 ;   Revision History:								    *
 ;										    *
 ;   1:	  Got everything for the gun working the way that I think it should with    *
-;	  base features.
-;										    *	
-;************************************************************************************		
+;	  base features.							    *
+;										    *
+;   2:	  I2C communications for enabling/disabling the target as well as reporting *
+;          any player hits to the target when the target is enabled.		    *
+;										    *
+;************************************************************************************
 		
 		#include "SLAVE.inc"      		; processor specific variable definitions
 		#INCLUDE <SLAVE_SETUP_001.inc>		; Custom setup file for the PIC16F883 micro-controller
-		#INCLUDE <SLAVE_SUBROUTINES.inc>		; File containing all used subroutines
+		#INCLUDE <SLAVE_SUBROUTINES.inc>	; File containing all used subroutines
 		LIST      p=16f1788		  	; list directive to define processor
 		errorlevel -302,-207,-305,-206,-203	; suppress "not in bank 0" message,  Found label after column 1,
 							; Using default destination of 1 (file),  Found call to macro in column 1
@@ -65,15 +63,15 @@ SETUP
 INTERRUPT
 		BANKSEL	    PIR1
 		BTFSC	    PIR1,3			;CHECK SSPIF FLAG TO SEE IF DATA WAS RECIEVED FROM MASTER
-		CALL	    RECEIVE
+		CALL	    RECEIVE			;CALL THE RECEIVE SUBROUTINE IF DATA WAS RECEIVED
 		BANKSEL	    PORTA
 		BANKSEL	    IOCBF
-		BTFSC	    IOCBF,1
-		GOTO	    RECORD_PLAYER2_HIT
-		BTFSC	    IOCBF,0
-		GOTO	    RECORD_PLAYER1_HIT
+		BTFSC	    IOCBF,1			;IF THE PLAYER 2 IR RECEIVER IS TRIGGERED (FALLING EDGE INTERRUPT ON CHANGE)
+		GOTO	    RECORD_PLAYER2_HIT		;CALL RECORD_PLAYER2_HIT SUBROUTINE
+		BTFSC	    IOCBF,0			;IF THE PLAYER 1 IR RECEIVER IS TRIGGERED (FALLING EDGE INTERRUPT ON CHANGE)
+		GOTO	    RECORD_PLAYER1_HIT		;CALL RECORD_PLAYER1_HIT SUBROUTINE
 	GOBACK
-		BANKSEL	    PIR1
+		BANKSEL	    PIR1			;NOTE: GOBACK ONLY USED IN I2C INTERRUPTS. IF USED FOR IOCBF, MISSES I2C REQUESTS AND MASTER CRASHES
 		BCF	    PIR1,3			;CLEAR SSP1IF
 		RETFIE					;RETURN TO MAIN, RE-ENABLE GIE
 ;Main Code
