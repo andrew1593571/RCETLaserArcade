6c00b12877dbacd51b05f93c5423024ad3b5b313
andrew1593571 <andrewkeller2002@gmail.com>
2025-10-21 14:44:36 -0600

    Apply new gitignore to remove KiCad backup and lock files

--- Assembly/Project_Target/Master/LA_Master.X/build/default/production/_ext/1472/LA_Master.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/1472/LA_Master.o:  \
-	C:/Users/Alex/OneDrive/Documents/Laser-Arcade-Project/Assembly/Project_Target/Master/LA_Master.asm  \
-	C:/Users/Alex/OneDrive/Documents/Laser-Arcade-Project/Assembly/Project_Target/Master/MASTER.inc  \
-	C:/Users/Alex/OneDrive/Documents/Laser-Arcade-Project/Assembly/Project_Target/Master/MASTER_SETUP.inc  \
-	C:/Users/Alex/OneDrive/Documents/Laser-Arcade-Project/Assembly/Project_Target/Master/MASTER_SUBROUTINES.inc  \
-
